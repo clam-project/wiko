8072f8f8c2112425a61cc5e27f26a4f7470e76b3
David García Garzón <532178+vokimon@users.noreply.github.com>
2007-12-08 19:49:45 +0000

    * generate_figures: specifying directories on the command line (default '.' and 'img')  * generate_figures: function for error messages

--- wiko/generate_figures.py
+++ wiko/generate_figures.py
@@ -50,21 +50,15 @@ def norun(command, verbose=False) :
 	return True
 def phase(name) :
 	print "\033[32m== %s\033[0m" % name
+def error(message) :
+	print >> sys.stderr, "\033[31m%s\033[0m" % message
 
-def main() :
-	if "-h" in sys.argv or "--help" in sys.argv :
-		print >> sys.stderr, "Usage: %s [-f|--force] [-v|--verbose] [-h|--help]"
-		print >> sys.stderr, "Options:"
-		print >> sys.stderr, "\t--force    Forces regeneration"
-		print >> sys.stderr, "\t--verbose  Prints executed commands and reports uptodate images"
-		print >> sys.stderr, "\t--help     Prints this help"
-		exit(0)
-
-	forceRebuild = "-f" in sys.argv or "--force" in sys.argv
-	verbose = "-v" in sys.argv or "--verbose" in sys.argv
-
+def generateFiguresOnDirectory(dir, forceRebuild, verbose) :
+	if dir == "" : dir = "."
+	if not os.access(dir, os.X_OK) :
+		return error("Directory '%s' is not accessible"%dir)
 	for fromSuffix, toSuffix, commands in converters :
-		sources = glob.glob("*"+fromSuffix)
+		sources = glob.glob(os.path.join(dir,"*"+fromSuffix))
 		for source in sources :
 			basename = os.path.splitext(source)[0]
 			target = basename + toSuffix
@@ -82,8 +76,30 @@ def main() :
 				)
 			for command in commands :
 				if run(command % vars, verbose) : continue
-				print >> sys.stderr, "\033[31mCommand failed!\033[0m"
+				error("Command failed!")
 				break
+
+def main() :
+	if "-h" in sys.argv or "--help" in sys.argv :
+		print >> sys.stderr, "Usage: %s [-f|--force] [-v|--verbose] [-h|--help]"
+		print >> sys.stderr, "Options:"
+		print >> sys.stderr, "\t--force    Forces regeneration"
+		print >> sys.stderr, "\t--verbose  Prints executed commands and reports uptodate images"
+		print >> sys.stderr, "\t--help     Prints this help"
+		return
+	forceRebuild = "-f" in sys.argv or "--force" in sys.argv
+	verbose = "-v" in sys.argv or "--verbose" in sys.argv
+	dirs = [dir for dir in sys.argv[1:] 
+		if dir not in [
+			'-v', '--verbose',
+			'-f', '--force',
+			'-h', '--help',
+		]]
+	if not dirs : dirs = ["", "img"]
+
+	for dir in dirs :
+		generateFiguresOnDirectory(dir, forceRebuild, verbose)
+
 
 if __name__=="__main__" :
 	main()

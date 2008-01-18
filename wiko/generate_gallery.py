dd32a47ff16f285d8a2f476494c8748d1cc03623
vokimon <vokimon@90b3e349-1917-499e-88f1-a4f5a529e452>
2008-01-18 07:48:02 +0000

    * generate_gallery:  ** the page is generated on the current directory instead of the one containing the images  ** generalized to many parametrized galleries  ** the name is in a different row than the image, this way they are aligned  ** using os.path.join in it should be used

--- wiko/generate_gallery.py
+++ wiko/generate_gallery.py
@@ -3,40 +3,60 @@ import glob
 import Image
 import os
 
-dir ="borra/"
-
-size=210
-prefix = "thumb_"
-columns=4
-
-files = [os.path.basename(filename) for filename in glob.glob(dir+"*.jpg")+glob.glob(dir+"*.png") if prefix not in filename]
-
-for imagefile in files:
-	im = Image.open(dir+imagefile)
-	im.thumbnail((size, size), Image.ANTIALIAS)
-	im.save(dir + prefix + imagefile, "JPEG")
-
-
-table = [
-	"<table> <tr>"
+galleries = [
+	# title, output, imageDir, template, columns, thumbsize
+	("Screenshots", "screenshots.html", "screenshots", "skeleton.html", 4, 210)
 ]
-count=1
-for filename in files:
-	table += ["<td style='text-align:center'><a href='%s' <img src='%s'/></a><br />%s </td>" % (
-		filename, 
-		prefix+filename,
-		filename[:-4])
+
+for title, output, imageDir, skeleton, columns, thumbSize in galleries :
+	thumbSize=210
+	prefix = "thumb_"
+	columns=4
+	formats = ["*.png", "*.jpg"]
+
+	files = [
+		os.path.basename(filename) 
+			for format in formats
+			for filename in glob.glob(os.path.join(imageDir,format))
+			if prefix not in filename ]
+
+	for imagefile in files:
+		im = Image.open(os.path.join(imageDir,imagefile))
+		im.thumbnail((thumbSize, thumbSize), Image.ANTIALIAS)
+		im.save(os.path.join(imageDir,prefix+imagefile), "JPEG")
+
+
+	table = [ "<table> <tr>" ]
+	labels = []
+	count=1
+	for filename in files:
+		print filename
+		table += ["<td style='text-align:center'><a href='%s' <img src='%s'/></a></td>" % (
+			os.path.join(imageDir,filename), 
+			os.path.join(imageDir,prefix+filename),
+			) ]
+		labels += ["<td style='text-align:center'>%s</td>" % filename[:-4] ]
+		if count == columns:
+			table += ["</tr>", "<tr>"]
+			table += labels
+			table += ["</tr>", "<tr>"]
+			labels = []
+			count=0
+		count+=1
+	if labels :
+			table += ["</tr>", "<tr>"]
+			table += labels
+	table += [
+		"</tr></table>"
 		]
-	if count == columns:
-		table += ["</tr>", "<tr>"]
-		count=0
-	count+=1
 
-table += [
-	"</tr></table>"
-	]
-template = file(os.path.expanduser("skeleton_gallery.html")).read()
-content = "\n".join(table)
-index = template%{'title': 'screenshots', 'content': content, 'author':'generated'}
+	template = file(os.path.expanduser(skeleton)).read()
 
-file(dir+"/index.html","w").write( index )
+	import re
+	context = dict( [(item.group(1),"") for item in re.compile(r"%\((.+?)\)s").finditer(template) ] )
+	content = "\n".join(table)
+	context.update({'title': title, 'content': content, 'author':'WiKo'})
+
+	file(output,"w").write( template % context )
+
+

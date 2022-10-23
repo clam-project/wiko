dacd02d6d440dcb81a2d428f8345fa0b35eac14a
David García Garzón <david.garcia@somenergia.coop>
2022-10-23 15:29:39 +0200

    * wiko_gallery/dowload_area: code in main function

--- wiko/generate_download_area.py
+++ wiko/generate_download_area.py
@@ -32,29 +32,32 @@ def humanReadableTime(filename) :
 	return str(datetime.datetime.fromtimestamp(os.path.getmtime(filename)))
 
 
-template = file(os.path.expanduser("scheleton_download.html")).read()
+def main():
 
-for title, dirname in dirs:
-	dirname = os.path.expanduser(dirname)
-	if not os.access(dirname, os.X_OK):
-		print "Not available: %s"%dirname
-		continue
-	print "Generating index for %s"%dirname
-	files = os.listdir(dirname)
-	files = filter((lambda a : a not in blacklist ), files) # Filter blacklisted
-	dirs = [dir+"/" for dir in filter((lambda a : os.path.isdir(os.path.join(dirname, a)) ), files) ]
-	files = filter((lambda a : not os.path.isdir(os.path.join(dirname, a)) ), files) # Filter dirs
-	files.sort()
+	template = file(os.path.expanduser("scheleton_download.html")).read()
 
-	table = "\n".join( [ "<tr><td><a href='%(filename)s'>%(filename)s</a></td><td>%(size)s</td><td>%(date)s</td></tr>"%{
-			'filename': filename,
-			'size': humanReadableSize(os.path.join(dirname, filename)),
-			'date': humanReadableTime(os.path.join(dirname, filename)),
-		} for filename in dirs + files ])
-	content = "<h1> Downloads </h1>"
-	content += "<h2> %s </h2>"%title
-	content += "\n".join(["<table width='100%'>",table,"</table>"])
-	index = template%{'title': title, 'content': content, 'author':'generated'}
-	open(os.path.join(dirname,"index.html"),"w").write(index)
+	for title, dirname in dirs:
+		dirname = os.path.expanduser(dirname)
+		if not os.access(dirname, os.X_OK):
+			print "Not available: %s"%dirname
+			continue
+		print "Generating index for %s"%dirname
+		files = os.listdir(dirname)
+		files = filter((lambda a : a not in blacklist ), files) # Filter blacklisted
+		dirs = [dir+"/" for dir in filter((lambda a : os.path.isdir(os.path.join(dirname, a)) ), files) ]
+		files = filter((lambda a : not os.path.isdir(os.path.join(dirname, a)) ), files) # Filter dirs
+		files.sort()
 
+		table = "\n".join( [ "<tr><td><a href='%(filename)s'>%(filename)s</a></td><td>%(size)s</td><td>%(date)s</td></tr>"%{
+				'filename': filename,
+				'size': humanReadableSize(os.path.join(dirname, filename)),
+				'date': humanReadableTime(os.path.join(dirname, filename)),
+			} for filename in dirs + files ])
+		content = "<h1> Downloads </h1>"
+		content += "<h2> %s </h2>"%title
+		content += "\n".join(["<table width='100%'>",table,"</table>"])
+		index = template%{'title': title, 'content': content, 'author':'generated'}
+		open(os.path.join(dirname,"index.html"),"w").write(index)
 
+if __name__=='__main__':
+	main()
--- wiko/generate_gallery.py
+++ wiko/generate_gallery.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 import glob
-import Image
+from PIL import Image
 import os
 
 galleries = [
@@ -8,55 +8,59 @@ galleries = [
 	("Screenshots", "screenshots.html", "screenshots", "skeleton.html", 4, 210)
 ]
 
-for title, output, imageDir, skeleton, columns, thumbSize in galleries :
-	thumbSize=210
-	prefix = "thumb_"
-	columns=4
-	formats = ["*.png", "*.jpg"]
+def main():
+	for title, output, imageDir, skeleton, columns, thumbSize in galleries :
+		thumbSize=210
+		prefix = "thumb_"
+		columns=4
+		formats = ["*.png", "*.jpg"]
 
-	files = [
-		os.path.basename(filename) 
-			for format in formats
-			for filename in glob.glob(os.path.join(imageDir,format))
-			if prefix not in filename ]
+		files = [
+			os.path.basename(filename) 
+				for format in formats
+				for filename in glob.glob(os.path.join(imageDir,format))
+				if prefix not in filename ]
 
-	for imagefile in files:
-		im = Image.open(os.path.join(imageDir,imagefile))
-		im.thumbnail((thumbSize, thumbSize), Image.ANTIALIAS)
-		im.save(os.path.join(imageDir,prefix+imagefile), "JPEG")
+		for imagefile in files:
+			im = Image.open(os.path.join(imageDir,imagefile))
+			im.thumbnail((thumbSize, thumbSize), Image.ANTIALIAS)
+			im.convert('RGB').save(os.path.join(imageDir,prefix+imagefile), "JPEG")
 
 
-	table = [ "<table> <tr>" ]
-	labels = []
-	count=1
-	for filename in files:
-		print filename
-		table += ["<td style='text-align:center'><a href='%s' <img src='%s'/></a></td>" % (
-			os.path.join(imageDir,filename), 
-			os.path.join(imageDir,prefix+filename),
-			) ]
-		labels += ["<td style='text-align:center'>%s</td>" % filename[:-4] ]
-		if count == columns:
-			table += ["</tr>", "<tr>"]
-			table += labels
-			table += ["</tr>", "<tr>"]
-			labels = []
-			count=0
-		count+=1
-	if labels :
-			table += ["</tr>", "<tr>"]
-			table += labels
-	table += [
-		"</tr></table>"
-		]
+		table = [ "<table> <tr>" ]
+		labels = []
+		count=1
+		for filename in files:
+			print(filename)
+			table += ["<td style='text-align:center'><a href='%s'><img src='%s'/></a></td>" % (
+				os.path.join(imageDir,filename), 
+				os.path.join(imageDir,prefix+filename),
+				) ]
+			labels += ["<td style='text-align:center'>%s</td>" % filename[:-4] ]
+			if count == columns:
+				table += ["</tr>", "<tr>"]
+				table += labels
+				table += ["</tr>", "<tr>"]
+				labels = []
+				count=0
+			count+=1
+		if labels :
+				table += ["</tr>", "<tr>"]
+				table += labels
+		table += [
+			"</tr></table>"
+			]
 
-	template = file(os.path.expanduser(skeleton)).read()
+		template = open(os.path.expanduser(skeleton)).read()
 
-	import re
-	context = dict( [(item.group(1),"") for item in re.compile(r"%\((.+?)\)s").finditer(template) ] )
-	content = "\n".join(table)
-	context.update({'title': title, 'content': content, 'author':'WiKo'})
+		import re
+		context = dict( [(item.group(1),"") for item in re.compile(r"%\((.+?)\)s").finditer(template) ] )
+		content = "\n".join(table)
+		context.update({'title': title, 'content': content, 'author':'WiKo'})
 
-	file(output,"w").write( template % context )
+		open(output,"w").write( template % context )
 
+
+if __name__ == '__main__':
+	main()
 
